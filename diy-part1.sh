5b967cd32d6aad0efac0731364e606d53175caf1
plmcli <94753613+plmcli@users.noreply.github.com>
2022-04-24 13:30:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages'
-echo 'src-git luci https://github.com/coolsnowwolf/luci'
-echo 'src-git routing https://git.openwrt.org/feed/routing.git'
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git'
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
